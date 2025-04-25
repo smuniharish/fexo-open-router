9459151aa46eabc7549c1851d610883ea302861f
HARISHIMSS <harishm@imss.co.in>
2025-04-25 15:34:34 +0530

    updated

--- app/helpers/workers/solr_worker.py
+++ app/helpers/workers/solr_worker.py
@@ -10,7 +10,7 @@ BATCH_SIZE = 1000
 BATCH_TIME = 5
 MAX_QUEUE_SIZE = 100000
 
-queue: asyncio.Queue[Any] = asyncio.Queue()
+queue: asyncio.Queue[Any] = asyncio.Queue(maxsize=MAX_QUEUE_SIZE)
 batch: List[Any] = []
 batch_lock = asyncio.Lock()
 
@@ -19,23 +19,24 @@ async def solr_worker() -> None:
     """Worker that process the queue based on batch size or time"""
     logger.info("solr worker initialized")
     while True:
-        await asyncio.sleep(BATCH_TIME)
-        async with batch_lock:
-            if len(batch) >= BATCH_SIZE or (batch and not queue.empty()):
-                await batch_index_to_solr(batch)
-                batch.clear()
+        try:
+            item = await asyncio.wait_for(queue.get(),timeout=BATCH_TIME)
+            async with batch_lock:
+                batch.append(item)
+                if len(batch) >= BATCH_SIZE:
+                    await batch_index_to_solr(batch)
+                    batch.clear()
+        except asyncio.TimeoutError:
+            async with batch_lock:
+                if batch:
+                    await batch_index_to_solr(batch)
+                    batch.clear()
 
 
 async def add_to_queue(record: dict) -> Any:
     """Receive a record and queue it for batch indexing"""
-    while queue.qsize() >= MAX_QUEUE_SIZE:
-        logger.warning("Queue is full, waiting for space before adding more records.")
+    while queue.full():
+        logger.warning(f"Queue is full :: {queue.qsize()}, waiting...")
         await asyncio.sleep(2)
     await queue.put(record)
-    async with batch_lock:
-        batch.append(record)
-
-        if len(batch) >= BATCH_SIZE:
-            await batch_index_to_solr(batch)
-            batch.clear()
     return record
