e18236aa0ceb57e797074119703d7a9ce6dcf3b3
HARISHIMSS <harishm@imss.co.in>
2025-06-26 14:22:02 +0530

    updates checked - 26/06/2025

--- app/routers/solr_helpers.py
+++ app/routers/solr_helpers.py
@@ -12,29 +12,6 @@ from app.helpers.Enums.mongo_status_enum import MongoStatusEnum
 
 router = APIRouter(prefix="/solr-helpers", tags=["solr-helpers"])
 
-
-# @router.get("/solr-docs-status")
-# async def solr_docs_status() -> dict[str, int]:
-#     statuses = [
-#         MongoStatusEnum.NEW,
-#         MongoStatusEnum.QUEUED,
-#         MongoStatusEnum.ERRORED,
-#         MongoStatusEnum.INDEXED,
-#     ]
-
-#     raw_results = await asyncio.gather(*[get_documents_count_with_status(status) for status in statuses])
-#     print("raw_results",raw_results)
-
-#     # Extract count from result list like [{'count': 1}]
-#     def extract_count(result: list[dict]) -> int:
-#         if isinstance(result, list) and result and isinstance(result[0], dict) and "count" in result[0]:
-#             return int(result[0]["count"])
-#         return 0
-
-#     results = [extract_count(res) for res in raw_results]
-#     return {status.value: count for status, count in zip(statuses, results, strict=False)}
-
-
 @router.get("/solr-docs-status")
 async def solr_docs_status() -> dict[str, Any]:
     statuses = [
@@ -77,9 +54,16 @@ async def download_docs(
     writer.writeheader()
     writer.writerows(docs)
     csv_buffer.seek(0)
+    file_name=f"{status}_docs.csv"
+    if(start_time):
+        file_name=f"{status}_{start_time}_docs.csv"
+    if(end_time):
+        file_name=f"{status}_{end_time}_docs.csv"
+    if start_time and end_time:
+        file_name = f"{status}_{start_time}_{end_time}_docs.csv"
 
     return StreamingResponse(
         csv_buffer,
         media_type="text/csv",
-        headers={"Content-Disposition": f"attachment; filename={status}_docs.csv"}
+        headers={"Content-Disposition": f"attachment; filename={file_name}"}
     )
